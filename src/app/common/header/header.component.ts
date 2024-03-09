5a33bfc9734fcbdda6f437063c4823a522d34064
Chaima <chaima.akremi.1997@gmail.com>
2024-03-09 15:18:36 +0100

    [EVOL] - Update menu by connected user

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,20 +1,44 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, RouterLink } from '@angular/router';
 import { Router } from '@angular/router';
+import { filter } from 'rxjs';
+import { LocalStorageService } from '../../api-services/local-storage.service';
+import { ConstsHelper } from '../../consts.helper';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    CommonModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  isAdministrator: boolean;
 
   constructor(
     public router: Router,
+    public localStorageService: LocalStorageService,
   ) {
+  }
+
+  ngOnInit() {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
+      // display administration or customer menu by listening to route
+      if (this.localStorageService.getToken() && this.localStorageService.getToken().role === ConstsHelper.ROLE_ADMINISTRATOR) {
+        this.isAdministrator = true;
+      } else {
+        this.isAdministrator = false;
+      }
+    });
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['login']);
   }
 }
