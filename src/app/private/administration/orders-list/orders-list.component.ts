0863634ed68166fe188a999a883ad17ee1fbd931
Chaima <chaima.akremi.1997@gmail.com>
2024-03-12 23:05:00 +0100

    [EVOL] - Update order status

--- src/app/private/administration/orders-list/orders-list.component.ts
+++ src/app/private/administration/orders-list/orders-list.component.ts
@@ -3,17 +3,25 @@ import { OrdersService } from '../../../api-services/orders.service';
 import { OrderInterface } from '../../../models/order.interface';
 import { OrderFilterInterface } from '../../../models/order-filter.interface';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
 import { StatusService } from '../../../api-services/status.service';
 import { StatusInterface } from '../../../models/status.interface';
 
 @Component({
   selector: 'app-orders-list',
   standalone: true,
-    imports: [
-        CommonModule,
-        FormsModule
-    ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './orders-list.component.html',
   styleUrl: './orders-list.component.css'
 })
@@ -24,29 +32,52 @@ export class OrdersListComponent implements OnInit {
   orders: Array<OrderInterface> = [];
   selectedOrder: OrderInterface|null;
   filter: OrderFilterInterface;
+  displaySearchForm: boolean = false;
+  formSubmitted: boolean = false;
+  form: FormGroup;
+  control: FormControl;
 
   constructor(
     private ordersService: OrdersService,
     private statusService: StatusService,
+    private formBuilder: FormBuilder,
   ) {
   }
 
   ngOnInit() {
     this.selectedOrder = null;
     this.selectedStatusId = null;
+    this.statusService.getList().subscribe(
+      response => {
+        this.statusList = response;
+        this.initForm();
+        this.getOrdersList(null, null, null);
+      }
+    );
+  }
+
+  initForm() {
+    this.control = this.formBuilder.control('', Validators.required);
+    this.form = this.formBuilder.group({});
+    this.form.addControl('userName', this.formBuilder.control('', []));
+    this.form.addControl('statusId', this.formBuilder.control('', []));
+    this.form.addControl('date', this.formBuilder.control('', []));
+  }
+
+  getOrdersList(userName: string|null, statusId: string|null, date: string|null) {
+    if (date) {
+      const splitDate = date.split('/');
+      date = splitDate[2] + '-' + splitDate[1] + '-' + splitDate[0];
+    }
+
     this.filter = {
-      userName: null,
-      statusId: null,
-      date: null,
+      userName: [null, ''].indexOf(userName) != -1 ? null : userName,
+      statusId: [null, ''].indexOf(statusId) != -1 ? null : parseInt(statusId),
+      date: [null, ''].indexOf(date) != -1 ? null : date,
     };
     this.ordersService.getAllOrders(this.filter).subscribe(ordersResponse => {
       this.orders = ordersResponse;
     });
-    this.statusService.getList().subscribe(
-      response => {
-        this.statusList = response;
-      }
-    );
   }
 
   setSelectedOrder(order: OrderInterface) {
@@ -60,5 +91,23 @@ export class OrdersListComponent implements OnInit {
         window.location.reload()
       }
     );
+  }
+
+  searchFormVisibility(display: boolean) {
+    this.displaySearchForm = display;
+  }
+
+  search() {
+    this.formSubmitted = true;
+    if (this.form.valid) {
+      this.getOrdersList(this.form.get('userName').value, this.form.get('statusId').value, this.form.get('date').value);
+    }
+  }
+
+  reset() {
+    this.formSubmitted = false;
+    this.searchFormVisibility(false);
+    this.initForm();
+    this.getOrdersList(null, null, null);
   }
 }
